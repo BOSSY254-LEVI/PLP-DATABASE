0baeb56e528455937648b4effb6bd60cd3d91748
BOSSY254-LEVI <livingstoneoduory@gmail.com>
2025-08-01 23:35:39 +0300

    database sales

---
+++ answers.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE salesDB; -- This query creates a database named salesDB
+
+-- Before droping a database, we must ensure that it is created, so we'll first create a database named demo. 
+
+CREATE DATABASE demo; -- This query creates a database named demo.
+
+DROP DATABASE demo; --- Then this query drops the database named demo, which we just created.
+
+--For the first question, I used the CREATE DATABASE statement to make a new database named "salesDB". This is the standard SQL syntax for creating databases.
+--For the second question, I first created a database then I used the DROP DATABASE statement to permanently delete the database named "demo". 
+
